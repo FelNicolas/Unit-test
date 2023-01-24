b9a6fe09bb20510a981a89aafbb44b1a174ab4b9
Nicolas <nicolasfelb@gmail.com>
2023-01-24 17:44:16 +0200

    test1

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -15,7 +15,7 @@ namespace ClassLibrary1
         }
         public int Add(int a, int b) 
         {
-            return a+b;
+            return a+b+1;
         }
 
         public string Something() 
